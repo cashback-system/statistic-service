4ad51c614ff3b3ff6bdc519175901ca1418946c2
Vurdolak2012 <slava.gusarov.73@mail.ru>
2024-03-02 20:22:00 +0300

    Init service

---
+++ src/test/java/cashback/system/statistic/service/StatisticServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package cashback.system.statistic.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
