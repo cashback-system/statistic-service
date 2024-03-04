b937a0cff08489c0fb54017b58052de3354e34b3
Vyach12 <slava.gusarov.73@mail.ru>
2024-03-04 21:21:23 +0300

    added transfer submodule

--- src/main/java/cashback/system/statistic/service/StatisticServiceApplication.java
+++ src/main/java/cashback/system/statistic/service/StatisticServiceApplication.java
@@ -3,8 +3,15 @@ package cashback.system.statistic.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Это не хайп. swing лучше
+ */
 @SpringBootApplication
 public class StatisticServiceApplication {
+	/**
+	 * Это хайп
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(StatisticServiceApplication.class, args);
 	}
