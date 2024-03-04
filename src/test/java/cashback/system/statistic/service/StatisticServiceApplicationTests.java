805b48100d7dc543814da609bc4a3a2bb3129439
Vyach12 <slava.gusarov.73@mail.ru>
2024-03-04 21:33:51 +0300

    [fix]: fixed git submodule

--- src/test/java/cashback/system/statistic/service/StatisticServiceApplicationTests.java
+++ src/test/java/cashback/system/statistic/service/StatisticServiceApplicationTests.java
@@ -2,8 +2,10 @@ package cashback.system.statistic.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StatisticServiceApplicationTests {
 	@Test
 	void contextLoads() {
